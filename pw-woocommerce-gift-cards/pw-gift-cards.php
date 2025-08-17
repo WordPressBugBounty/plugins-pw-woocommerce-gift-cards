30c3e5bb97955ad79b5e81501ffe452198bff3a0
Sean Murphy <sean@iamseanmurphy.com>
2025-08-17 20:18:37 +0000

    2.17

--- pw-woocommerce-gift-cards/pw-gift-cards.php
+++ pw-woocommerce-gift-cards/pw-gift-cards.php
@@ -3,13 +3,13 @@
  * Plugin Name: PW WooCommerce Gift Cards
  * Plugin URI: https://www.pimwick.com/gift-cards/
  * Description: Sell gift cards in your WooCommerce store.
- * Version: 2.16
+ * Version: 2.17
  * Author: Pimwick, LLC
  * Author URI: https://www.pimwick.com
  * Text Domain: pw-woocommerce-gift-cards
  * Domain Path: /languages
  * WC requires at least: 4.0
- * WC tested up to: 9.8
+ * WC tested up to: 10.1
  * Requires Plugins: woocommerce
 */
 
@@ -108,7 +108,7 @@ add_action( 'plugins_loaded', function() {
         return;
     }
 
-                                                                                                                                                                                                                                                                define( 'PWGC_VERSION', '2.16' );
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                define( 'PWGC_VERSION', '2.17' );
 
     load_plugin_textdomain( 'pw-woocommerce-gift-cards', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
--- pw-woocommerce-gift-cards/readme.txt
+++ pw-woocommerce-gift-cards/readme.txt
@@ -5,7 +5,7 @@ Tags: woocommerce, gift cards, gift certificates, vouchers, store credit
 Requires at least: 4.5
 Tested up to: 6.8
 Requires PHP: 7.4
-Stable tag: 2.16
+Stable tag: 2.17
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -94,6 +94,9 @@ The following currency switcher plugins are supported:
 
 == Changelog ==
 
+= 2.17 =
+* Compatibility with WooCommerce 10.1.
+
 = 2.16 =
 * Added integration with YayCurrency currency switcher.
 
@@ -121,15 +124,12 @@ The following currency switcher plugins are supported:
 = 2.8 =
 * Fixed an issue with new installations where the tables aren't automatically created.
 
-= 2.7 =
-* Compatibility fix for translations in WordPress 6.7 Updated readme.txt file to include Polish translation. Prevent an exception when upgrading to the Pro version when the free version is still activated.
-
 = Previous versions =
 * See changelog.txt
 
 == Upgrade Notice ==
 
-= 2.16 =
-* Added integration with YayCurrency currency switcher.
+= 2.17 =
+* Compatibility with WooCommerce 10.1.
 
 
